47da764b68fba4b6999b435a7cab482c794acaf5
hw3842765 <hw3842765@gmail.com>
2025-01-03 14:19:12 +0300

    updated

--- gemeni.py
+++ gemeni.py
@@ -1,10 +1,12 @@
 import subprocess
 import json
 
+api_key = "YOUR-API-KEY"
+
 def ollama_api(prompt, model):
     try:
         output = subprocess.check_output([
-            "curl", "-s", "-X", "POST", "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyCc01pIA69-ng3RJeGzl4Mu9UPDCdHIChE", 
+            "curl", "-s", "-X", "POST", f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={api_key}", 
             "-H", "Content-Type: application/json", 
             "-d", json.dumps({
                 "contents": [{
